994154f4f2950e374a512214d56f01c5c7a73cb1
Anoop Singh <anoop102910@gmail.com>
2023-10-24 12:13:01 +0530

    fixed small errors

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:5000", withCredentials: true });
+export const api = axios.create({ baseURL: "https://socially-api-u2p6.onrender.com", withCredentials: true });
