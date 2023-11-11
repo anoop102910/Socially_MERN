315c8bcd36b7adea0faca3ec3514425e47df0a7f
Anoop Singh <anoop102910@gmail.com>
2023-11-11 18:16:17 +0530

    fixed some bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // darkMode: "class",
+  darkMode: "dark-mode",
   // darkMode:"false",
 
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
